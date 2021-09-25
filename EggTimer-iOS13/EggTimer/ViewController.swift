b77366f47b9e33c23a5787a91d1e0ba221be2042
kimjiin <kj980926@naver.com>
2021-09-25 23:39:09 +0900

    타이머 설정 및 퍼센트 변화 설정

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,16 +7,68 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
-    let eggsTitle = ["Soft": 5, "Medium": 7, "Hard": 10]
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggsTitle = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
+//
+//    override func viewDidLoad() {
+//            super.viewDidLoad()
+//
+//        progressBar.clipsToBounds = true
+//        progressBar.layer.cornerRadius = 8
+//        progressBar.clipsToBounds = true
+//        progressBar.layer.sublayers![1].cornerRadius = 8// 뒤에 있는 회색 track
+//        progressBar.subviews[1].clipsToBounds = true
+//
+//        }
+
     
     @IBAction func buttonClicked(_ sender: UIButton) {
+        timer.invalidate()
         let title = sender.currentTitle!
-        let result = eggsTitle[title] //타입이 옵셔널임
-        print(result!) //그러므로 출력할 때 ! 붙임
+        totalTime = eggsTitle[title]!
+        
+        //버튼 누를때 초기화
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = title
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+
+    //update progress
+    @objc func updateCounter() {
+        //example functionality
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+
+            let persectProgress = Float(secondsPassed) / Float(totalTime)
+            print(persectProgress)
+            progressBar.progress = persectProgress
+            
+        }else{
+            timer.invalidate()
+            playSound()
+            titleLabel.text = "Egg's are Ready!"
+            
+        }
     }
     
+    func playSound() {
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
 
 }
