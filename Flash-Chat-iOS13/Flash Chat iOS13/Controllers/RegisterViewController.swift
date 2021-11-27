fb41b012c986885b366f97843dac7f1d85020dd9
kimjiin <kj980926@naver.com>
2021-11-27 15:54:06 +0900

    [FlashChat] Firebase 사용해서 Login, Register

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,17 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        //2개 옵셔널 체이닝 해서 이메일과 비번 있을 때만 밑에 실행
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else { //error가 nil이면
+                    //Navigate to the ChatViewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
