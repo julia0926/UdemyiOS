7453fed596be63f4c285d95be0a6d14a1728f1ce
kimjiin <kj980926@naver.com>
2021-11-27 16:09:12 +0900

    [FlashChat] Logout, 화면이동 정리

---
+++ Flash-Chat-iOS13/MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+
+var now = Date().timeIntervalSince1970
+let oneSecondFromNow = now + 1
+
+while now < oneSecondFromNow {
+    now = Date().timeIntervalSince1970
+    
+    print(now)
+}
