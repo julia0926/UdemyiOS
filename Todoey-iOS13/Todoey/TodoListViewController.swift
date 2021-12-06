ea4f125a1bd86347bdf273b30468adf37abe272c
kimjiin <kj980926@naver.com>
2021-12-06 19:29:47 +0900

    [Todoey] Init & TableView 구성

---
+++ Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    
+    let itemArray = ["Find Mike", "Buy Choco", "Study Hard"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //L : method의 파라미터로 tableView
+        //V : Variable으로 외부에서 쓰이는, global한
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+}
+
